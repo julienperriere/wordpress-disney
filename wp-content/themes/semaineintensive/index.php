cd02d21c152d8efe2605dacdd2d58b1ab4391590
Julien Perrière <perrierejulien@gmail.com>
2013-04-22 11:32:08 +0200

    Upload home page and fix merge issues

--- wp-content/themes/semaineintensive/index.php
+++ wp-content/themes/semaineintensive/index.php
@@ -1,4 +1,141 @@
 <!-- Header -->
 <?php get_header(); ?>
+<<<<<<< HEAD
+
+<section id="topNews">
+	<ul id="topNewsSlider">
+		<li>
+			<a href="#">
+				<img src="img/topNews1.jpg" alt="" />
+				<h1>Lorem ipsum dolor sit amet consectetur !</h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</p>
+			</a>
+		</li>
+	</ul>
+	<ul id="topNewsControls">
+		<li><a href="#" class="topNewsControlsActive"></a></li>
+		<li><a href="#"></a></li>
+		<li><a href="#"></a></li>
+		<li><a href="#"></a></li>
+		<li><a href="#"></a></li>
+	</ul>
+</section>
+
+<section id="characters">
+	<h1>Les personnages Disney</h1>
+	<ul>
+		<li>
+			<a href="#">
+				<figure>
+				  <img src="img/character1.jpg" alt="" />
+				  <span class="characterOverlay"></span>
+				  <figcaption><span class="characterArrow"></span>Jack Sparrow</figcaption>
+				</figure>
+			</a>
+		</li>
+		<li>
+			<a href="#">
+				<figure>
+				  <img src="img/character1.jpg" alt="" />
+				  <span class="characterOverlay"></span>
+				  <figcaption><span class="characterArrow"></span>Jack Sparrow</figcaption>
+				</figure>
+			</a>
+		</li>
+		<li>
+			<a href="#">
+				<figure>
+				  <img src="img/character1.jpg" alt="" />
+				  <span class="characterOverlay"></span>
+				  <figcaption><span class="characterArrow"></span>Jack Sparrow</figcaption>
+				</figure>
+			</a>
+		</li>
+		<li>
+			<a href="#">
+				<figure>
+				  <img src="img/character1.jpg" alt="" />
+				  <span class="characterOverlay"></span>
+				  <figcaption><span class="characterArrow"></span>Jack Sparrow</figcaption>
+				</figure>
+			</a>
+		</li>
+	</ul>
+</section>
+
+<section id="filmsByGenre">
+	<hgroup>
+		<h1>Les films par genre</h1>
+		<h2><a href="#">Voir tous les films &raquo;</a></h2>
+	</hgroup>
+	<ul>
+		<li>
+			<a href="#">
+				<figure>
+				  <img src="img/poster1.jpg" alt="" />
+				  <figcaption>Action et Aventure</figcaption>
+				</figure>
+			</a>
+		</li>
+		<li>
+			<a href="#">
+				<figure>
+				  <img src="img/poster1.jpg" alt="" />
+				  <figcaption>Action et Aventure</figcaption>
+				</figure>
+			</a>
+		</li>
+		<li>
+			<a href="#">
+				<figure>
+				  <img src="img/poster1.jpg" alt="" />
+				  <figcaption>Action et Aventure</figcaption>
+				</figure>
+			</a>
+		</li>
+		<li>
+			<a href="#">
+				<figure>
+				  <img src="img/poster1.jpg" alt="" />
+				  <figcaption>Action et Aventure</figcaption>
+				</figure>
+			</a>
+		</li>
+		<li>
+			<a href="#">
+				<figure>
+				  <img src="img/poster1.jpg" alt="" />
+				  <figcaption>Action et Aventure</figcaption>
+				</figure>
+			</a>
+		</li>
+		<li>
+			<a href="#">
+				<figure>
+				  <img src="img/poster1.jpg" alt="" />
+				  <figcaption>Action et Aventure</figcaption>
+				</figure>
+			</a>
+		</li>
+		<li>
+			<a href="#">
+				<figure>
+				  <img src="img/poster1.jpg" alt="" />
+				  <figcaption>Action et Aventure</figcaption>
+				</figure>
+			</a>
+		</li>
+		<li>
+			<a href="#">
+				<figure>
+				  <img src="img/poster1.jpg" alt="" />
+				  <figcaption>Action et Aventure</figcaption>
+				</figure>
+			</a>
+		</li>
+	</ul>
+</section>
+=======
+>>>>>>> 0b9eacb1bbe164eafd2fd0d9821b246b2f00a459
 	
 <?php get_footer(); ?>
